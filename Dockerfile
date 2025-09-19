32ac4050cdbe3434fd9e1196a29a4ef74fd5dbb6
Cédric Guetté <cedric.guette@gmail.com>
2025-09-19 09:12:05 +0200

    correction des en-têtes qui ne prenaient pas en compte la variable d'environnement

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM openjdk:21-jdk-slim-bullseye
-
-WORKDIR /app-back
-
-COPY target/*.jar /app-back/portfiolio.jar
-
-ENTRYPOINT ["java", "-jar", "app-back/portfolio.jar"]
--- src/main/java/com/cedric_guette/portfolio/configuration/HttpHeadersCORS.java
+++ src/main/java/com/cedric_guette/portfolio/configuration/HttpHeadersCORS.java
@@ -1,12 +1,17 @@
 package com.cedric_guette.portfolio.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 
 public class HttpHeadersCORS {
+
+    @Value("${app.url-front}")
+    private String urlFront;
+
     public HttpHeaders headers() {
         HttpHeaders httpHeader = new HttpHeaders();
         httpHeader.set("Access-Control-Allow-Headers","Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token");
-        httpHeader.set("Access-Control-Allow-Origin","http://localhost:3000");
+        httpHeader.set("Access-Control-Allow-Origin",urlFront);
         httpHeader.set("Access-Control-Allow-Methods", "DELETE,GET,HEAD,OPTIONS,PUT,POST,PATCH");
 
         return httpHeader;
